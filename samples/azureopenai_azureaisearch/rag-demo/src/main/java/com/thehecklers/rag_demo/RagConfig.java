59b021db7ecac707bb33336f24f19e966f9b86a1
Mark Heckler <mark.heckler@gmail.com>
2024-08-22 16:02:50 -0500

    Added Azure OpenAI + Azure AI Search sample app

--- samples/azureopenai_azureaisearch/rag-demo/src/main/java/com/thehecklers/rag_demo/RagConfig.java
+++ samples/azureopenai_azureaisearch/rag-demo/src/main/java/com/thehecklers/rag_demo/RagConfig.java
@@ -1,4 +1,32 @@
 package com.thehecklers.rag_demo;
 
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.search.documents.indexes.SearchIndexClient;
+import com.azure.search.documents.indexes.SearchIndexClientBuilder;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.azure.AzureVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RagConfig {
+    @Value("${spring.ai.vectorstore.azure.initialize-schema:false}")
+    private boolean initializeSchema;
+
+    private final String endPoint = System.getenv("AZURE_AI_SEARCH_ENDPOINT");
+    private final String apiKey = System.getenv("AZURE_AI_SEARCH_API_KEY");
+
+    @Bean
+    public SearchIndexClient searchIndexClient() {
+        return new SearchIndexClientBuilder().endpoint(endPoint)
+                .credential(new AzureKeyCredential(apiKey))
+                .buildClient();
+    }
+
+    @Bean
+    public VectorStore vectorStore(SearchIndexClient searchIndexClient, EmbeddingModel embeddingModel) {
+        return new AzureVectorStore(searchIndexClient, embeddingModel, initializeSchema);
+    }
 }
---
+++ samples/azureopenai_azureaisearch/rag-demo/src/main/java/com/thehecklers/rag_demo/RagDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.rag_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagDemoApplication.class, args);
+	}
+
+}

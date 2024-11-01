66bc5e20e0eb9e7e6d02f8aaf1ad47bf849ed50e
Mark Heckler <mark.heckler@gmail.com>
2024-11-01 14:23:57 -0500

    Updated to resolve RAG issues

---
+++ samples/openai_neo4j/rag-demo/src/main/java/com/thehecklers/rag_demo/NeoConfig.java
@@ -0,0 +1,22 @@
+package com.thehecklers.rag_demo;
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NeoConfig {
+    @Bean
+    public EmbeddingModel embeddingModel() {
+        // Can be any other EmbeddingModel implementation.
+        return new OpenAiEmbeddingModel(new OpenAiApi(System.getenv("SPRING_AI_OPENAI_API_KEY")));
+    }
+
+    /*@Bean
+    public Driver driver() {
+        return GraphDatabase.driver("neo4j://<host>:<bolt-port>",
+                AuthTokens.basic("<username>", "<password>"));
+    }*/
+}

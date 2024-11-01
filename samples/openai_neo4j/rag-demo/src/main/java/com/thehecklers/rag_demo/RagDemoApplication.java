97f6e3231331005280769f26c06f5aeff300d96f
Jennifer Reif <jennifer.reif@neo4j.com>
2024-11-01 09:39:29 -0500

    Add neo4j sample project

---
+++ samples/openai_neo4j/rag-demo/src/main/java/com/thehecklers/rag_demo/RagDemoApplication.java
@@ -0,0 +1,12 @@
+package com.thehecklers.rag_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagDemoApplication.class, args);
+	}
+}
